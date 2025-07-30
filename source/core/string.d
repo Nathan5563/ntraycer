6408d4c534de460143c96f32240d468da650b098
Nathan5563 <ndawit611@gmail.com>
2025-07-29 20:53:51 -0400

    feat: Added custom String and related utils

---
+++ source/core/string.d
@@ -0,0 +1,56 @@
+module core.string;
+
+struct String
+{
+    char[] buf;
+    size_t size;
+
+    this(string...)(const string args)
+    {
+        this.append(args);
+    }
+
+    char opIndex(size_t idx) const
+    {
+        return this.buf[idx];
+    }
+
+    void append(string...)(const string args)
+    {
+        foreach (s; args)
+        {
+            size_t newSize = this.size + s.length;
+            if (this.buf.length < newSize)
+            {
+                size_t newLength;
+                if (this.buf.length == 0)
+                {
+                    newLength = 128;
+                }
+                else
+                {
+                    newLength = this.buf.length;    
+                }
+
+                while (newLength < newSize)
+                {
+                    newLength *= 2;
+                }
+                this.buf.length = newLength;
+            }
+            foreach (idx, ch; s)
+            {
+                this.buf[this.size + idx] = ch;
+            }
+            this.size += s.length;
+        }
+    }
+    private string toString() const
+    {
+        return (this.buf[0 .. this.size]).idup; 
+    }
+
+    alias toString this;
+}
+
+// TODO: UNIT TESTS
---
+++ source/core/utils.d
@@ -0,0 +1,52 @@
+module core.utils;
+
+import math.utils : abs;
+
+template isIntegral(T)
+{
+    enum bool isIntegral = 
+        is(T == byte) ||
+        is(T == ubyte) ||
+        is(T == short) ||
+        is(T == ushort) ||
+        is(T == int) ||
+        is(T == uint) ||
+        is(T == long) ||
+        is(T == ulong) ||
+        is(T == char) ||
+        is(T == wchar) ||
+        is(T == dchar);
+}
+
+template isFloatingPoint(T)
+{
+    enum bool isFloatingPoint = 
+        is(T == float) ||
+        is(T == double) ||
+        is(T == real);
+}
+
+string intToString(const int value)
+{
+    long v = value;
+    char[32] buf;
+    size_t size = buf.length;
+
+    bool neg = v < 0;
+    v = abs(v);
+    do
+    {
+        buf[--size] = cast(char)('0' + (v % 10));
+        v /= 10;
+    }
+    while (v > 0);
+
+    if (neg)
+    {
+        buf[--size] = '-';
+    }
+
+    return buf[size .. $].idup;
+}
+
+// TODO: UNIT TESTS
