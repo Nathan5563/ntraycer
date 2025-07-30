8851befd69b9db301592260630fe4a078e68ed12
Nathan5563 <ndawit611@gmail.com>
2025-07-29 20:54:28 -0400

    feat: Impl'd custom syscall wrappers, made test image

--- source/app.d
+++ source/app.d
@@ -1,9 +1,32 @@
-import std.stdio;
-import std.file;
+import core.file;
+import core.string;
 
-import math.all;
+import core.utils : intToString;
 
 void main()
 {
-	// TODO: IMPLEMENT RAY TRACER
+	int width = 255;
+	int height = 255;
+
+	int fd = sys_open("image.ppm", O_WRONLY | O_CREAT | O_TRUNC, 420);
+	assert(!(fd < 0));
+	
+	String image = String("P3\n", intToString(height), " ", intToString(width), "\n255\n");
+	foreach (row; 0 .. width)
+	{
+		foreach (col; 0 .. height)
+		{
+			image.append(intToString(row), " ", intToString(col), " 0  ");
+			if ((col + 1) % 3 == 0)
+			{
+				image.append("\n");
+			}
+		}
+	}
+
+	ptrdiff_t nbytes = sys_write(fd, image.buf.ptr, image.size);
+	assert(nbytes == image.size);
+
+	int ret = sys_close(fd);
+	assert(!(ret < 0));
 }
---
+++ source/core/file.d
@@ -0,0 +1,53 @@
+module core.file;
+
+enum SYS_READ  = 0;
+enum SYS_WRITE = 1;
+enum SYS_OPEN  = 2;
+enum SYS_CLOSE = 3;
+
+enum O_WRONLY = 0x1;
+enum O_CREAT  = 0x40;
+enum O_TRUNC  = 0x200;
+
+int sys_open(const char* path, int flags, int mode)
+{
+    int fd;
+    asm
+    {
+        mov RAX, SYS_OPEN;
+        mov RDI, path;
+        mov RSI, flags;
+        mov RDX, mode;
+        syscall;
+        mov fd, RAX;
+    }
+    return fd;
+}
+
+ptrdiff_t sys_write(int fd, const void* buf, size_t count) {
+    ptrdiff_t ret;
+    asm
+    {
+        mov RAX, SYS_WRITE;
+        mov RDI, fd;
+        mov RSI, buf;
+        mov RDX, count;
+        syscall;
+        mov ret, RAX;
+    }
+    return ret;
+}
+
+int sys_close(int fd) {
+    int ret;
+    asm
+    {
+        mov RAX, SYS_CLOSE;
+        mov RDI, fd;
+        syscall;
+        mov ret, RAX;
+    }
+    return ret;
+}
+
+// TODO: UNIT TESTS
