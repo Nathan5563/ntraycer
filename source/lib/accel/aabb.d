e82192dbe91721d3bd92ed2695335798aa9a6da2
Nathan Abebe <ndawit611@gmail.com>
2026-01-24 17:27:52 -0500

    Implemented aabb and bvh acceleration

---
+++ source/lib/accel/aabb.d
@@ -0,0 +1,228 @@
+// COMPLETE
+/**
+ * This module provides an Axis-Aligned Bounding Box (AABB) implementation
+ * for use in spatial acceleration structures like BVH.
+ */
+module lib.accel.aabb;
+
+import lib.core.math : Vec3, Ray, min, max;
+
+/// @struct AABB - Axis-Aligned Bounding Box for spatial acceleration
+struct AABB
+{
+    Vec3 minPoint;
+    Vec3 maxPoint;
+
+    /// @func this - Constructs an AABB from two corner points
+    ///
+    /// @param minP - the minimum corner (smallest x, y, z values)
+    /// @param maxP - the maximum corner (largest x, y, z values)
+    this(Vec3 minP, Vec3 maxP)
+    {
+        this.minPoint = minP;
+        this.maxPoint = maxP;
+    }
+
+    /// @func empty - Creates an empty (invalid) AABB where min > max
+    static AABB empty()
+    {
+        return AABB(
+            Vec3(float.max, float.max, float.max),
+            Vec3(-float.max, -float.max, -float.max)
+        );
+    }
+
+    /// @func fromPoint - Creates an AABB containing a single point
+    ///
+    /// @param point - the point to enclose
+    static AABB fromPoint(Vec3 point)
+    {
+        return AABB(point, point);
+    }
+
+    /// @func hit - Tests ray-box intersection using the slab method
+    ///
+    /// @param ray - the ray to test
+    /// @param tMin - minimum t value for valid hits
+    /// @param tMax - maximum t value for valid hits
+    bool hit(Ray ray, float tMin, float tMax) const
+    {
+        // X axis slab
+        float invD = 1.0f / ray.direction.x;
+        float t0 = (minPoint.x - ray.origin.x) * invD;
+        float t1 = (maxPoint.x - ray.origin.x) * invD;
+        if (invD < 0.0f)
+        {
+            float temp = t0;
+            t0 = t1;
+            t1 = temp;
+        }
+        tMin = t0 > tMin ? t0 : tMin;
+        tMax = t1 < tMax ? t1 : tMax;
+        if (tMax <= tMin)
+            return false;
+
+        // Y axis slab
+        invD = 1.0f / ray.direction.y;
+        t0 = (minPoint.y - ray.origin.y) * invD;
+        t1 = (maxPoint.y - ray.origin.y) * invD;
+        if (invD < 0.0f)
+        {
+            float temp = t0;
+            t0 = t1;
+            t1 = temp;
+        }
+        tMin = t0 > tMin ? t0 : tMin;
+        tMax = t1 < tMax ? t1 : tMax;
+        if (tMax <= tMin)
+            return false;
+
+        // Z axis slab
+        invD = 1.0f / ray.direction.z;
+        t0 = (minPoint.z - ray.origin.z) * invD;
+        t1 = (maxPoint.z - ray.origin.z) * invD;
+        if (invD < 0.0f)
+        {
+            float temp = t0;
+            t0 = t1;
+            t1 = temp;
+        }
+        tMin = t0 > tMin ? t0 : tMin;
+        tMax = t1 < tMax ? t1 : tMax;
+        if (tMax <= tMin)
+            return false;
+
+        return true;
+    }
+
+    /// @func merge - Computes the union of two AABBs
+    ///
+    /// @param other - the other AABB to merge with
+    AABB merge(AABB other) const
+    {
+        return AABB(
+            Vec3(
+                min(minPoint.x, other.minPoint.x),
+                min(minPoint.y, other.minPoint.y),
+                min(minPoint.z, other.minPoint.z)
+            ),
+            Vec3(
+                max(maxPoint.x, other.maxPoint.x),
+                max(maxPoint.y, other.maxPoint.y),
+                max(maxPoint.z, other.maxPoint.z)
+            )
+        );
+    }
+
+    /// @func expandToInclude - Expands this AABB to include a point
+    ///
+    /// @param point - the point to include
+    AABB expandToInclude(Vec3 point) const
+    {
+        return AABB(
+            Vec3(
+                min(minPoint.x, point.x),
+                min(minPoint.y, point.y),
+                min(minPoint.z, point.z)
+            ),
+            Vec3(
+                max(maxPoint.x, point.x),
+                max(maxPoint.y, point.y),
+                max(maxPoint.z, point.z)
+            )
+        );
+    }
+
+    /// @func centroid - Returns the center point of this AABB
+    Vec3 centroid() const
+    {
+        return (minPoint + maxPoint) * 0.5f;
+    }
+
+    /// @func size - Returns the size (extent) along each axis
+    Vec3 size() const
+    {
+        return maxPoint - minPoint;
+    }
+
+    /// @func surfaceArea - Returns the surface area (for SAH heuristic)
+    float surfaceArea() const
+    {
+        Vec3 d = size();
+        return 2.0f * (d.x * d.y + d.y * d.z + d.z * d.x);
+    }
+
+    /// @func longestAxis - Returns index of longest axis (0=x, 1=y, 2=z)
+    int longestAxis() const
+    {
+        Vec3 d = size();
+        if (d.x > d.y && d.x > d.z)
+            return 0;
+        else if (d.y > d.z)
+            return 1;
+        else
+            return 2;
+    }
+}
+
+unittest
+{
+    import lib.core.math : fequals;
+
+    // Test AABB construction
+    AABB box = AABB(Vec3(-1, -2, -3), Vec3(1, 2, 3));
+    assert(box.minPoint.x == -1);
+    assert(box.maxPoint.z == 3);
+
+    // Test empty AABB
+    AABB empty = AABB.empty();
+    assert(empty.minPoint.x == float.max);
+    assert(empty.maxPoint.x == -float.max);
+
+    // Test fromPoint
+    AABB point = AABB.fromPoint(Vec3(5, 5, 5));
+    assert(point.minPoint == point.maxPoint);
+    assert(point.minPoint.x == 5);
+
+    // Test centroid
+    Vec3 center = box.centroid();
+    assert(fequals(center.x, 0));
+    assert(fequals(center.y, 0));
+    assert(fequals(center.z, 0));
+
+    // Test size
+    Vec3 sz = box.size();
+    assert(fequals(sz.x, 2));
+    assert(fequals(sz.y, 4));
+    assert(fequals(sz.z, 6));
+
+    // Test longestAxis
+    assert(box.longestAxis() == 2);  // Z is longest (6)
+
+    // Test merge
+    AABB box2 = AABB(Vec3(0, 0, 0), Vec3(5, 5, 5));
+    AABB merged = box.merge(box2);
+    assert(merged.minPoint.x == -1);
+    assert(merged.maxPoint.x == 5);
+
+    // Test expandToInclude
+    AABB expanded = box.expandToInclude(Vec3(10, 10, 10));
+    assert(expanded.maxPoint.x == 10);
+    assert(expanded.minPoint.x == -1);
+
+    // Test ray hit - ray through center
+    Ray ray = Ray(Vec3(0, 0, -10), Vec3(0, 0, 1));
+    assert(box.hit(ray, 0.0f, float.max) == true);
+
+    // Test ray miss
+    Ray missRay = Ray(Vec3(10, 10, -10), Vec3(0, 0, 1));
+    assert(box.hit(missRay, 0.0f, float.max) == false);
+
+    // Test ray hit from inside
+    Ray insideRay = Ray(Vec3(0, 0, 0), Vec3(1, 0, 0));
+    assert(box.hit(insideRay, 0.0f, float.max) == true);
+
+    // Test surface area
+    AABB unitBox = AABB(Vec3(0, 0, 0), Vec3(1, 1, 1));
+    assert(fequals(unitBox.surfaceArea(), 6.0f));
+}
---
+++ source/lib/accel/bvh.d
@@ -0,0 +1,187 @@
+// COMPLETE
+/**
+ * This module provides a Bounding Volume Hierarchy (BVH) implementation
+ * for accelerating ray-scene intersection tests.
+ */
+module lib.accel.bvh;
+
+import lib.accel.aabb : AABB;
+import lib.core.math : Vec3, Ray;
+import lib.scene.hittable.hittable : Hittable, HitInfo;
+
+/// @interface Boundable - Interface for objects that can provide a bounding box
+interface Boundable
+{
+    /// @func boundingBox - Returns the axis-aligned bounding box of this object
+    AABB boundingBox() const;
+}
+
+/// @class BVHNode - A node in the Bounding Volume Hierarchy tree
+class BVHNode : Hittable
+{
+    private AABB box;
+    private Hittable left;
+    private Hittable right;
+    private Hittable primitive;
+
+    /// @func this - Constructs a BVH from an array of hittable objects
+    ///
+    /// @param objects - array of hittable objects to build the BVH from
+    this(Hittable[] objects)
+    {
+        if (objects.length == 0)
+        {
+            box = AABB.empty();
+            return;
+        }
+
+        if (objects.length == 1)
+        {
+            primitive = objects[0];
+            Boundable b = cast(Boundable) objects[0];
+            if (b !is null)
+                box = b.boundingBox();
+            else
+                box = AABB(Vec3(-float.max, -float.max, -float.max), 
+                          Vec3(float.max, float.max, float.max));
+            return;
+        }
+
+        box = AABB.empty();
+        foreach (obj; objects)
+        {
+            Boundable b = cast(Boundable) obj;
+            if (b !is null)
+                box = box.merge(b.boundingBox());
+        }
+
+        int axis = box.longestAxis();
+        Hittable[] sorted = sortByAxis(objects, axis);
+        size_t mid = sorted.length / 2;
+
+        left = new BVHNode(sorted[0 .. mid]);
+        right = new BVHNode(sorted[mid .. $]);
+    }
+
+    /// @func sortByAxis - Sorts objects by bounding box centroid along axis
+    ///
+    /// @param objects - array of hittable objects to sort
+    /// @param axis - axis to sort along (0=x, 1=y, 2=z)
+    private static Hittable[] sortByAxis(Hittable[] objects, int axis)
+    {
+        Hittable[] result = new Hittable[objects.length];
+        foreach (i, obj; objects)
+            result[i] = obj;
+
+        for (size_t i = 1; i < result.length; i++)
+        {
+            Hittable key = result[i];
+            float keyVal = getCentroidAxis(key, axis);
+            
+            size_t j = i;
+            while (j > 0 && getCentroidAxis(result[j - 1], axis) > keyVal)
+            {
+                result[j] = result[j - 1];
+                j--;
+            }
+            result[j] = key;
+        }
+
+        return result;
+    }
+
+    /// @func getCentroidAxis - Gets the centroid of an object along a specific axis
+    ///
+    /// @param obj - the hittable object
+    /// @param axis - axis to get centroid for (0=x, 1=y, 2=z)
+    private static float getCentroidAxis(Hittable obj, int axis)
+    {
+        Boundable b = cast(Boundable) obj;
+        if (b is null)
+            return 0.0f;
+
+        Vec3 centroid = b.boundingBox().centroid();
+        if (axis == 0)
+            return centroid.x;
+        else if (axis == 1)
+            return centroid.y;
+        else
+            return centroid.z;
+    }
+
+    /// @func boundingBox - Returns the bounding box of this BVH node
+    AABB boundingBox() const
+    {
+        return box;
+    }
+
+    /// @func hit - Tests if a ray hits any object in this BVH subtree
+    ///
+    /// @param r - the ray to test
+    /// @param timeMin - minimum t value for valid hits
+    /// @param timeMax - maximum t value for valid hits
+    /// @param hitInfo - output hit information if a hit is found
+    bool hit(Ray r, float timeMin, float timeMax, out HitInfo hitInfo) const
+    {
+        if (!box.hit(r, timeMin, timeMax))
+            return false;
+
+        if (primitive !is null)
+            return primitive.hit(r, timeMin, timeMax, hitInfo);
+
+        HitInfo leftHit, rightHit;
+        bool hitLeft = left !is null && left.hit(r, timeMin, timeMax, leftHit);
+        bool hitRight = right !is null && right.hit(r, timeMin, hitLeft ? leftHit.time : timeMax, rightHit);
+
+        if (hitRight)
+        {
+            hitInfo = rightHit;
+            return true;
+        }
+        else if (hitLeft)
+        {
+            hitInfo = leftHit;
+            return true;
+        }
+
+        return false;
+    }
+}
+
+/// @class BVHAccelerator - A scene accelerated with a BVH
+class BVHAccelerator : Hittable
+{
+    private BVHNode root;
+
+    /// @func this - Constructs a BVH accelerator from an array of objects
+    ///
+    /// @param objects - the hittable objects to accelerate
+    this(Hittable[] objects)
+    {
+        root = new BVHNode(objects);
+    }
+
+    /// @func hit - Tests if a ray hits any object in the accelerated scene
+    ///
+    /// @param r - the ray to test
+    /// @param timeMin - minimum t value for valid hits
+    /// @param timeMax - maximum t value for valid hits
+    /// @param hitInfo - output hit information if a hit is found
+    bool hit(Ray r, float timeMin, float timeMax, out HitInfo hitInfo) const
+    {
+        if (root is null)
+            return false;
+        return root.hit(r, timeMin, timeMax, hitInfo);
+    }
+}
+
+unittest
+{
+    import lib.core.math : fequals;
+
+    // Test empty BVH
+    BVHAccelerator emptyBVH = new BVHAccelerator([]);
+    HitInfo hitInfo;
+    Ray ray = Ray(Vec3(0, 0, -5), Vec3(0, 0, 1));
+    assert(emptyBVH.hit(ray, 0.001f, float.max, hitInfo) == false);
+}
